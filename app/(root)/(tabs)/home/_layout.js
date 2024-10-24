a71a6131243bdd545649cb4260131de5ae8db33a
ChanBHPS21773 <117168997+ChanBHPS21773@users.noreply.github.com>
2024-10-24 20:32:26 +0700

    fix lỗi warn

--- app/(root)/(tabs)/home/_layout.js
+++ app/(root)/(tabs)/home/_layout.js
@@ -5,7 +5,7 @@ import { Stack } from 'expo-router'
 const _layout = () => {
   return (
    <Stack>
-    <Stack.Screen name="home" options={{ title: "Home", headerShown: false }} />
+    <Stack.Screen name="index" options={{ title: "Home", headerShown: false }} />
     <Stack.Screen name="productdetail" options={{ title: "productdetail", headerShown: false }} />
    </Stack>
   )
